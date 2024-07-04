3682bc597b537730848657f9bde936334fecd85f
kimjansheden <35255751+kimjansheden@users.noreply.github.com>
2024-07-04 20:42:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ga-optimizer",
-    version="0.1.1",
+    version="0.1.2",
     description="Gradient Accumulation Optimizer Wrapper for Keras/TensorFlow",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
